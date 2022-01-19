0fda7f3b3ed79bd2e628850778108adc2b353644
Fatima Rahman <fatimarahman@Fatimas-MacBook-Pro.local>
2022-01-18 21:35:49 -0500

    fixed html routes

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool
 require("dotenv").config();
 
 const devConfig = {
-  user: "me",
-  host: "localhost",
-  database: "inventorystore",
-  password: "password",
-  port: 5432,
+  user: PG_USER,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT,
 };
 
 const proConfig = {
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jsonParser = bodyParser.json();
 const PORT = process.env.PORT || 3000;
 const route = express.Router();
 const db = require("./db");
+const path = require('path');
+
 
 //middleware
 app.use(cors());
@@ -16,27 +18,23 @@ app.use(jsonParser);
 app.use('/', require('./httproutes'))
 
 app.get('/create/products',function(req,res){
-  res.sendFile('create.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, '/client', '/html', 'create.html'));
 });
 
 app.get('/view/products',function(req,res){
-  res.sendFile('view.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, '/client', '/html', 'view.html'));
 });
 
 app.get('/view/products/types',function(req,res){
-  res.sendFile('filter.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, '/client', '/html', 'filter.html'));
 });
 
 app.get('/edit/:id',function(req,res){
-  res.sendFile('edit.html', { root: __dirname });
-});
-
-app.get('/delete/products/:id',function(req,res){
-  res.sendFile('delete.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, '/client', '/html', 'edit.html'))
 });
 
 app.get('/',function(req,res){
-  res.sendFile('frontpage.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, '/client', '/html', 'frontpage.html'))
 });
 
 // app.use('/', route);
