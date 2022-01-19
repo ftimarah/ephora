69a5db4e775e3f118442008484909ae49b5fc1c1
Fatima Rahman <fatimarahman@Fatimas-MacBook-Pro.local>
2022-01-18 21:50:01 -0500

    removing env variables on db.js

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 const Pool = require('pg').Pool
 require("dotenv").config();
 
+// const devConfig = {
+//   user: PG_USER,
+//   host: PG_HOST,
+//   database: PG_DATABASE,
+//   password: PG_PASSWORD,
+//   port: PG_PORT,
+// };
+
 const devConfig = {
-  user: PG_USER,
-  host: PG_HOST,
-  database: PG_DATABASE,
-  password: PG_PASSWORD,
-  port: PG_PORT,
+  user: "me",
+  host: "localhost",
+  database: "inventorystore",
+  password: "password",
+  port: 5432,
 };
 
 const proConfig = {
